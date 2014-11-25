22537653465abbd29685496e1be2fbc91068883a
Ethan Keiser <eak65@drexel.edu>
2014-11-25 00:20:49 -0500

    Initial Commit

---
+++ GoRatio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoRatio
+//
+//  Created by Ethan Keiser on 11/25/14.
+//  Copyright (c) 2014 Ethan Keiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoRatio/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoRatio
+//
+//  Created by Ethan Keiser on 11/25/14.
+//  Copyright (c) 2014 Ethan Keiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
